777822df496853b134cc474f08e84873d27c5a68
chudaa203 <chudaa203@gmail.com>
2023-07-21 15:33:23 -0400

    added opacity to background image

--- test.js
+++ test.js
@@ -12,37 +12,61 @@ function createParagraph() {
   var eleventhWord = document.getElementById("word11").value;
   var twelvethWord = document.getElementById("word12").value;
 
-  var paragraph =
-    "Once, a " +
+  var paragraph = 
+  "Once, a " +
+    '<span class="highlight">' +
     thirdWord +
+    "</span>" +
     " " +
+    '<span class="highlight">' +
     firstWord +
+    "</span>" +
     " named Whiskers traveled to a magical forest. Whiskers stumbled upon a group of mischievous " +
+    '<span class="highlight">' +
     secondWord +
+    "</span>" +
     " who had a crazy dance battle" +
     "." +
     " The squirrels spun, " +
+    '<span class="highlight">' +
     fourthWord +
+    "</span>" +
     "," +
     " and " +
+    '<span class="highlight">' +
     twelvethWord +
+    "</span>" +
     " uncontrollably. Whiskers couldn't " +
+    '<span class="highlight">' +
     eighthWord +
+    "</span>" +
     " joining in and showcased some impressive breakdancing moves." +
     " The " +
+    '<span class="highlight">' +
     seventhWord +
+    "</span>" +
     " animals cheered and laughed as " +
+    '<span class="highlight">' +
     sixthWord +
+    "</span>" +
     " outperformed the " +
+    '<span class="highlight">' +
     ninethWord +
+    "</span>" +
     "." +
     " The hilarious spectacle attracted a crowd of amazed " +
+    '<span class="highlight">' +
     tenthWord +
+    "</span>" +
     "," +
     " including a juggling elephant and a singing kangaroo. Together, they formed an unforgettable impromptu performance that left everyone in stitches. The uproarious laughter echoed through the forest, creating a moment of " +
+    '<span class="highlight">' +
     eleventhWord +
+    "</span>" +
     " joy. " +
+    '<span class="highlight">' +
     fifthWord +
+    "</span>" +
     " and their new friends continued to dance and entertain, making the magical forest a place of laughter and happiness" +
     "." +
     "<br>" +
